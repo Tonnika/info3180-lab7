c0235b4442d5a9d0006eaacf16f43828de926caa
Tonnika Richards <shantiwilliams11@gmail.com>
2019-04-01 01:18:01 +0000

    exercise 1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,7 @@
+from flask_wtf import Form 
+from wtforms import TextArea, FileField 
+from wtforms.validators import Length
 
+class UploadForm(Form):
+    description = TextArea('Description', Length(max=80))
+    photo = FileField('Photo') 
